49b32d636264bb886b7036361f85a125a4935326
Yamina B <32742273+yamina1@users.noreply.github.com>
2023-01-17 14:07:46 +0000

    Update factorial.py

--- mymath/factorial.py
+++ mymath/factorial.py
@@ -10,5 +10,5 @@ def factorial(n):
 
     factorial = 1
     for i in range(1, n + 1):
-        #factorial = factorial * i
-    return factorial * i
+        factorial = factorial * i
+    return factorial
